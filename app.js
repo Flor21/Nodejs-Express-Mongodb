3ea05ebc78596c4958346e15e59d2dc571b6ae87
Flor21 <fas-2196@hotmail.com>
2020-06-02 12:37:02 -0300

    init proyecto red bicicletas

---
+++ app.js
@@ -0,0 +1,24 @@
+/*const http = require("http");
+const hostname = "0.0.0.0";
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hola mundo!\n");
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Andando http://${hostname}:${port}`);
+});*/
+
+/*var express = require("express");
+var app = express();
+
+app.get("/", function (req, res) {
+  res.send("Hola Mundo!!");
+});
+
+app.listen(3000, function () {
+  console.log("Aplicacion, escuchando en el puerto 3000!");
+});*/
---
+++ express.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker exec -ti cursoNodeExpMongo /home/node/app/node_modules/.bin/express '$@'
